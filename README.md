4ff4af48faddbac6b3629339ed50033f5d86736a
Yuto Watanabe <yuto.w51942@gmail.com>
2020-05-31 23:36:40 +0900

    refactoring and add readme

--- README.md
+++ README.md
@@ -3,3 +3,89 @@
 ## tl;dr
 
 - 各地の震度、震源地をマップに描画する
+
+## 使い方
+
+### インストール
+
+必要なもの
+
+- Node.js
+- yarn
+
+今回上記2つのインストール方法は割愛する。
+
+```bash
+# yarn、nodeはインストールされている状態とする
+cd map-draw
+yarn
+
+# 実行
+node src/mapping.js -i ~~~ -o ~~~~
+
+```
+
+### 引数の説明
+
+- `--input`, `-i`
+  - 震源地、各地の震度の情報（json形式）
+  - フォーマットの解説はこちら
+- `--output`, `-o`
+  - 生成後のファイルの保存先
+
+## 細かい設定
+
+生成する画像サイズや倍率、色の変更は
+
+[config/config.json](config/config.json)
+
+で、設定することができます。
+
+### フォーマット
+
+- `width`
+  - 生成する画像の横幅。Pixel
+- `height`
+  - 生成する画像の縦幅。Pixel
+- `scale`
+  - 倍率。桁が大きくなるほど拡大されます。
+  - 拡大率が低い場合、自動的に解像度が低くなります。
+    - `100 ≦ scale` で指定してください。
+- `sea_color`
+  - 海の色。
+  - デフォルトは、 ![color](https://via.placeholder.com/16/1a1a1a/FFFFFF/?text=%20) `#1a1a1a`
+- `land_color`
+  - 陸の色。
+  - デフォルトは、 ![color](https://via.placeholder.com/16/595959/FFFFFF/?text=%20) `#595959`
+- `stroke_color`
+  - 線の色。
+  - デフォルトは、 ![color](https://via.placeholder.com/16/ffffff/FFFFFF/?text=%20) `#ffffff`
+- `map`
+  - 使用する`geojson`ファイルのパス。
+- `seismic_intensity_color`
+  - 各震度の色
+    | Jsonでの呼び名 |  名前   |                                     色                                     |
+    | :------------: | :-----: | :------------------------------------------------------------------------: |
+    |      `0`       |  震度0  | ![color](https://via.placeholder.com/16/d9d9d9/FFFFFF/?text=%20) `#d9d9d9` |
+    |      `1`       |  震度1  | ![color](https://via.placeholder.com/16/2d1fcc/FFFFFF/?text=%20) `#2d1fcc` |
+    |      `2`       |  震度2  | ![color](https://via.placeholder.com/16/3b93db/FFFFFF/?text=%20) `#3b93db` |
+    |      `3`       |  震度3  | ![color](https://via.placeholder.com/16/67e071/FFFFFF/?text=%20) `#67e071` |
+    |      `4`       |  震度4  | ![color](https://via.placeholder.com/16/e2eb38/FFFFFF/?text=%20) `#e2eb38` |
+    |   `under_5`    | 震度5弱 | ![color](https://via.placeholder.com/16/e38227/FFFFFF/?text=%20) `#e38227` |
+    |    `over_5`    | 震度5強 | ![color](https://via.placeholder.com/16/e38227/FFFFFF/?text=%20) `#e38227` |
+    |   `under_6`    | 震度6弱 | ![color](https://via.placeholder.com/16/e81c2d/FFFFFF/?text=%20) `#e81c2d` |
+    |    `over_6`    | 震度6強 | ![color](https://via.placeholder.com/16/e81c2d/FFFFFF/?text=%20) `#e81c2d` |
+    |      `7`       |  震度7  | ![color](https://via.placeholder.com/16/db1d95/FFFFFF/?text=%20) `#db1d95` |
+
+- `epicenter_color`
+  - 震源地の色
+  - デフォルトは、 ![color](https://via.placeholder.com/16/d10026/FFFFFF/?text=%20) `#d10026`
+
+## 謝辞
+
+以下の記事を参考にさせていただきました。ありがとうございます。
+
+- [気象庁防災情報XMLとD3.jsを使って地震の震度分布図を作る](https://qiita.com/icchi_h/items/bbf563e1a7acec97a0e0)
+- [d3.js + jsdomで国土地理院のベクトルタイルからSVGファイルを生成](https://qiita.com/cieloazul310/items/a8e776bbe8a70262df99)
+
+色調は、[特務機関NERV防災](https://nerv.app/)を参考にさせていただきました。ありがとうございます。
--- src/mapping.js
+++ src/mapping.js
@@ -12,7 +12,7 @@ const simplify = require('simplify-geojson');
 const jsdom = require('jsdom');
 
 const { JSDOM } = jsdom;
-const document = new JSDOM(``).window.document;
+const document = new JSDOM(`<link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@500&display=swap" rel="stylesheet">`).window.document;
 
 
 // --- Read args ---
@@ -26,17 +26,20 @@ const options = commandLineArgs(optionDefinitions)
 config = JSON.parse(fs.readFileSync('config/config.json'));
 
 // --- Setting ---
-const area_info = JSON.parse(fs.readFileSync(options.input)); // Epicenter, area and its depth.
-const save_path = options.output;                             // The path to save.
-const width = config.width;                                   // Image width.
-const height = config.height;                                 // Image height.
-const def_scale = config.scale;                               // The magnification of the map.
-const sea_color = config.sea_color;                           // Sea color.
-const land_color = config.land_color;                         // Land color.
-const stroke_color = config.stroke_color;                     // Stroke color.
-const map = config.map;                                       // The map path in geojson format to use.
+const area_info = JSON.parse(fs.readFileSync(options.input));   // Epicenter, area and its depth.
+const save_path = options.output;                               // The path to save.
+const width = config.width;                                     // Image width.
+const height = config.height;                                   // Image height.
+const def_scale = config.scale;                                 // The magnification of the map.
+const sea_color = config.sea_color;                             // Sea color.
+const land_color = config.land_color;                           // Land color.
+const stroke_color = config.stroke_color;                       // Stroke color.
+const map = config.map;                                         // The map path in geojson format to use.
+const seismic_intensity_color = config.seismic_intensity_color; // Seismic intensity color.
+const epicenter_color = config.epicenter_color;                 // Epicrnter point colir.
 
 const epicenter = area_info.epicenter;                        // epicenter. [ longitude, latitude ]
+
 
 
 // --- Read geojson(map) file ---
@@ -81,25 +84,124 @@ q.awaitAll((err, files) => {
         .style('fill', land_color)
         .style('stroke', stroke_color);
 
+    // --- Seismic intensity of each place ---
+    const Export = (area, color, text) => {
+        coordinate = aProjection(area)
+        svg.append('circle')
+            .attr('r', 30)
+            .attr('cx', coordinate[0])
+            .attr('cy', coordinate[1])
+            .style('fill', color);
+
+        svg.append('text')
+            .text(text)
+            .attr('x', coordinate[0])
+            .attr('y', coordinate[1] + 20)
+            .attr('font-size', 60)
+            .attr('text-anchor', 'middle')
+            .attr('font-family', 'monospace');
+    };
+
+    // Seismic intensity 0
+    if (area_info.areas['0']) {
+        color = seismic_intensity_color['0']
+        text = '0'
+        for (let area of area_info.areas['0']) {
+            Export(area, color, text);
+        }
+
+    }
+    // Seismic intensity 1
+    if (area_info.areas['1']) {
+        color = seismic_intensity_color['1']
+        text = '1'
+        for (let area of area_info.areas['1']) {
+            Export(area, color, text);
+        }
+    }
+    // Seismic intensity 2
+    if (area_info.areas['2']) {
+        color = seismic_intensity_color['2']
+        text = '2'
+        for (let area of area_info.areas['2']) {
+            Export(area, color, text);
+        }
+    }
+    // Seismic intensity 3
+    if (area_info.areas['3']) {
+        color = seismic_intensity_color['3']
+        text = '3'
+        for (let area of area_info.areas['3']) {
+            Export(area, color, text);
+        }
+    }
+    // Seismic intensity 4
+    if (area_info.areas['4']) {
+        color = seismic_intensity_color['4']
+        text = '4'
+        for (let area of area_info.areas['4']) {
+            Export(area, color, text);
+        }
+    }
+    // Seismic intensity 5-
+    if (area_info.areas['under_5']) {
+        color = seismic_intensity_color['under_5']
+        text = '5-'
+        for (let area of area_info.areas['inder_5']) {
+            Export(area, color, text);
+        }
+    }
+    // Seismic intensity 5+
+    if (area_info.areas['over_5']) {
+        color = seismic_intensity_color['over_5']
+        text = '5+'
+        for (let area of area_info.areas['over_5']) {
+            Export(area, color, text);
+        }
+    }
+    // Seismic intensity 6-
+    if (area_info.areas['under_6']) {
+        color = seismic_intensity_color['under_6']
+        text = '6-'
+        for (let area of area_info.areas['under_6']) {
+            Export(area, color, text);
+        }
+    }
+
+    // Seismic intensity 6+
+    if (area_info.areas['over_6']) {
+        color = seismic_intensity_color['over_6']
+        text = '6+'
+        for (let area of area_info.areas['over_6']) {
+            Export(area, color, text);
+        }
+    }
+    // Seismic intensity 7
+    if (area_info.areas['7']) {
+        color = seismic_intensity_color['7']
+        text = '7'
+        for (let area of area_info.areas['7']) {
+            Export(area, color, text);
+        }
+    }
+
     // -- epicenter --
     center = aProjection(epicenter)
     svg.append('line')
-        .attr('x1', center[0] - 20)
-        .attr('x2', center[0] + 20)
-        .attr('y1', center[1] - 20)
-        .attr('y2', center[1] + 20)
-        .attr('stroke-width', 10)
-        .style('stroke', '#eb3b3b');
+        .attr('x1', center[0] - 30)
+        .attr('x2', center[0] + 30)
+        .attr('y1', center[1] - 30)
+        .attr('y2', center[1] + 30)
+        .attr('stroke-width', 20)
+        .style('stroke', epicenter_color);
 
     svg.append('line')
-        .attr('x1', center[0] - 20)
-        .attr('x2', center[0] + 20)
-        .attr('y1', center[1] + 20)
-        .attr('y2', center[1] - 20)
-        .attr('stroke-width', 10)
-        .style('stroke', '#eb3b3b');
-
-
+        .attr('x1', center[0] - 30)
+        .attr('x2', center[0] + 30)
+        .attr('y1', center[1] + 30)
+        .attr('y2', center[1] - 30)
+        .attr('stroke-width', 20)
+        .style('stroke', epicenter_color);
 
 
     // --- Save SVG file ----
